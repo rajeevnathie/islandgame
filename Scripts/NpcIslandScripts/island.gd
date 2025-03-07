3fdd0cf2f2df1adf5f38cfbd8d7e2964552b1a04
Jan Herscheid <jan@Jans-MacBook-Pro.local>
2025-03-07 22:33:35 +0100

    change cam pos n some other stuff I forgot

--- Scripts/NpcIslandScripts/island.gd
+++ Scripts/NpcIslandScripts/island.gd
@@ -3,7 +3,13 @@ class_name Island
 
 @onready var islandFloorMesh = get_node("RigidBody3D/MeshInstance3D")
 @onready var islandFloorCollision = get_node("RigidBody3D/CollisionShape3D")
-@export var Flora : int
+@export var climate : int
+@export var terrain : int
+@export var moisture : int
+@export var geoStability : int
+@export var biodiversity : int
+@export var elementalEnergy : int
+@export var successionStage : int
 
 func changeSize(size: Vector3):
 	islandFloorMesh.scale = size
