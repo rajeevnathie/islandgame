5661470fe744351d047b902e9ed304479c7beffa
rajeevnathie <44583817+rajeevnathie@users.noreply.github.com>
2025-03-08 12:18:06 +0100

    Fixed tree spawning errors

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,31 +1,20 @@
 extends Node3D
 
 var npc_island = preload("res://Scenes/npc_island.tscn")
-
-@export var tree_manager : PackedScene
 var island_factory = load("res://Scripts/island_factory.gd").new()
 
+@export var tree_manager : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(island_factory)
-	island_factory.instantiateIsland()
+	var island_instance = island_factory.instantiateIsland()
+	
+	var tree_manager_instance = tree_manager.instantiate()
+	tree_manager_instance.island_instance = island_instance
+	add_child(tree_manager_instance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func instantiateIsland():
-	var instance = npc_island.instantiate()
-	add_child(instance)
-	randomiseIsland(instance)
-	print("added island with size", instance.getSize())
-	
-	var tree_manager_instance = tree_manager.instantiate()
-	tree_manager_instance.island_instance = instance
-	add_child(tree_manager_instance)
-	
-func randomiseIsland(island : Island):
-	var rng = RandomNumberGenerator.new()
-	
-	island.changeSize(Vector3(rng.randf_range(1.0, 10.0), 0.2, randf_range(1.0, 10.0)))
